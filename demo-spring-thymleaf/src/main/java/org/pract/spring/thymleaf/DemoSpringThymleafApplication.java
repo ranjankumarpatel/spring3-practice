61ddecc2e0d7885d8c60a98bc2b75b89f2681a53
ranjan <ranjanpatelbbsr@gmail.com>
2017-03-12 12:40:43 +0530

    Signed-off-by: ranjan <ranjanpatelbbsr@gmail.com>

---
+++ demo-spring-thymleaf/src/main/java/org/pract/spring/thymleaf/DemoSpringThymleafApplication.java
@@ -0,0 +1,12 @@
+package org.pract.spring.thymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringThymleafApplication.class, args);
+	}
+}
